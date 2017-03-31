fcf83b69b09fbf2be54f5ce0d9b314213d7cd713
Sarah Bindman <sarahbindman@gmail.com>
2017-03-31 13:00:24 -0700

    tic tac toe and other intro to react tutorials

--- src/index.js
+++ src/index.js
@@ -3,114 +3,190 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
 
-var listAffirmations = [
-    "you are great",
-    "you are valued",
-    "you are important",
-    "your work matters",
-    "you are enough",
-    "You are so great",
-    " You got this ",
-    "You are awesome",
-    " You CAN do it",
-    "You are fantastic",
-    " You are worthy",
-    "You are wonderful",
-    "You are your greatest asset",
-    "You are a superstar",
-    "You have so much to be proud of",
-    "Think about how much you have grown",
-    "Don't be too hard on yourself",
-    "Failure is part of success" ,
-    "Don't sell yourself short",
-    "Don't stop, get it get it"
-];
+//creating a square component
+class Square extends React.Component {
 
+    render() {
+        return (
+            //onclick event is passed to the square from 2 parents up
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
 
+class Board extends React.Component {
 
+    renderSquare(i) {
+        //onclick event is tied to a specific square
+        return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    }
+    render() {
+        const status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
 
+class Game extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    }
 
-var AffirmElement = React.createClass ({
-    getInitialState: function() {
-        // Start off by selecting the first board
-        return {
-            selectedIndex: -1
+    handleClick(squareI) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[squareI]) {
+            return;
         }
-    },
-    affirmClick: function(evt) {
-        // Here's the meat of the problem. Notice how we can use this.props here (and anywhere else in the component).
-        // When this is called, React updates the state and updates the UI to reflect the new render output.
+        squares[squareI] = this.state.xIsNext ? 'X' : 'O';
+
         this.setState({
-            selectedIndex: this.state.selectedIndex < (listAffirmations.length - 1) ? this.state.selectedIndex + 1 : 0
+            squares : squares,
+            xIsNext : !this.state.xIsNext
         });
-    },
-    //get text for button
-    getAffirmButtonText : function(index) {
-        if (index >= 0) {
-            return "Another Affirmation!";
-        }
-        else {
-            return "Affirm Me";
-        }
-    },
+    }
 
-    render: function() {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board squares={this.state.squares} xIsNext={this.state.xIsNext} onClick={(i) => this.handleClick(i)}/>
+                </div>
+                <div className="game-info">
+                </div>
+            </div>
+        );
+    }
+}
 
-        var boards = [];
-        for (var i = 0; i < listAffirmations.length ; i++) {
-            // We can compare to state here so we're no longer always selecting the first board.
-            var isSelected = i === this.state.selectedIndex;
-            boards.push(
-                <Board index={i} selected={isSelected} key={i} />
-            );
-        }
 
+
+class Circle extends React.Component {
+
+    render() {
+        return (
+            <span className={"circle " + this.props.fillClass} onClick={() => this.props.onClick()}>
+            </span>
+        );
+    }
+}
+
+
+class StreetButton extends React.Component {
+
+    render() {
+        var label = this.props.pedX ? "Ped Xing" : "No Ped Xing";
+        return (
+            //onclick events must be tied to HTML elements not react components
+            <button className="streetButton" onClick={() => this.props.onClick()}  >
+                {label}
+            </button>
+        );
+    }
+}
+
+
+
+class StopLight extends React.Component {
+
+    renderCircle(i, fillClass) {
+        return <Circle value={i} fillClass={fillClass} onClick={() => this.props.onClick()}/>;
+    }
+    render() {
+        var fillRed = this.props.pedX ? 'red' : '';
+        var fillGreen = this.props.pedX ? '' : 'green';
 
         return (
             <div>
-                {/*<div className="boards">{boards}</div>*/}
-                <TextDisplay startIndex={this.state.selectedIndex} />
-
-                <button onClick={this.affirmClick} >
-                    {this.getAffirmButtonText(this.state.selectedIndex)}
-                </button>
-
-                <div>
-                    <div className="boards">{boards}</div>
+                <div className="board-row">
+                    {/*red*/}
+                    {this.renderCircle(0, fillRed)}
+                    {/*green*/}
+                    {this.renderCircle(1, fillGreen)}
                 </div>
             </div>
-
         );
     }
-});
+}
 
+class Intersection extends React.Component {
 
-var TextDisplay = React.createClass({
-
-    render: function() {
-
+    constructor() {
+        super();
+        this.state = {
+            pedX: false
+        };
+    }
+    handleClick() {
+        this.setState({
+            pedX : !this.state.pedX
+        });
+    }
+    render() {
         return (
-            <div className="textDisplay" >
-                {listAffirmations[this.props.startIndex]}
+            <div>
+                <StopLight pedX={this.state.pedX} onClick={() => this.handleClick()}/>
+                <StreetButton pedX={this.state.pedX} onClick={() => this.handleClick()}/>
             </div>
         );
     }
-});
+}
 
-var Board = React.createClass({
-    render: function() {
-        var className = "board";
-        if (this.props.selected) {
-            className += " selected";
-        }
-        return (
-            <div className={className}></div>
-        );
-    }
-});
+
+
+
+// ========================================
 
 ReactDOM.render(
-    <AffirmElement />,
-    document.getElementById('textBox')
+    <Game />,
+    document.getElementById('container')
 );
 
+
+ReactDOM.render(
+    <Intersection />,
+    document.getElementById('stop-light')
+);
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
